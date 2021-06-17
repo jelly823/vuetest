94481f430bb155e620364b54616c2e6e1a5b3c2f
jelly <jellyl823@163.com>
2021-06-17 11:00:16 +0800

    test

---
+++ 5communication/src/components/Child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    child <br />
+    {{ msg }} <br />
+    <button @click="handleClick">to parent</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["msg"],
+  methods: {
+    handleClick() {
+      this.$emit("to-parent", "emit from child");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 5communication/src/components/Parent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    parent
+    <Child :msg="msgForChild" @to-parent="handleToParent"></Child>
+  </div>
+</template>
+
+<script>
+import Child from "./Child.vue";
+
+export default {
+  components: {
+    Child,
+  },
+  data() {
+    return {
+      msgForChild: "msg from parent",
+    };
+  },
+  methods: {
+    handleToParent(data) {
+      alert(data);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
